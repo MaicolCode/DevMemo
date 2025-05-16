0f4b21497bcaf275bb8b17b938b6faf9fe25fc3d
MaicolCode <paucarjhon07@outlook.com>
2025-05-15 21:51:47 -0500

    Refactoring the function to get information from notes

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,12 +1,18 @@
+import { auth } from "@clerk/nextjs/server";
+
 export async function getNotes() {
-    const res = await fetch("http://localhost:3000/api/notes",{
+    const { getToken } = await auth();
+    const token = await getToken();
+
+    const res = await fetch("http://localhost:3000/api/notes", {
         method: "GET",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
         }
     });
 
-    if(!res.ok) throw new Error("Failed to fetch notes");
+    if (!res.ok) throw new Error((await res.json()).error);
 
     return res.json();
 }
