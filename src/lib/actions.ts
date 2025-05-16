d80f992db0457d226730345e5fc87b0d8a4e23a9
MaicolCode <paucarjhon07@outlook.com>
2025-05-15 21:06:50 -0500

    Create actions to get the notes from supabase with the created endpoints

---
+++ src/lib/actions.ts
@@ -0,0 +1,12 @@
+export async function getNotes() {
+    const res = await fetch("/api/notes",{
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if(!res.ok) throw new Error("Failed to fetch notes");
+
+    return res.json();
+}
