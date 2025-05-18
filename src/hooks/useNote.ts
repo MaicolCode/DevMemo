9ac9c65a5e6a23058f58a134045ecf5a7adc813c
MaicolCode <paucarjhon07@outlook.com>
2025-05-18 15:59:48 -0500

    Creation of new hooks to be used in components

--- src/hooks/useNote.ts
+++ src/hooks/useNote.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useUser } from '@clerk/nextjs';
-import { Note } from '@/types';
-import { getNotes } from '@/lib/actions';
+import { FormData, Note } from '@/types';
+import { getNotes, updateNote } from '@/lib/actions';
+import { useRouter } from 'next/navigation';
 
 export interface UseNoteResult {
   note: Note | null;
@@ -10,36 +11,76 @@ export interface UseNoteResult {
 }
 
 export function useNote() {
-  const {user} = useUser();
+  const { user } = useUser();
   const id = user?.id;
   const [notes, setNotes] = useState<Note[] | null>(null);
-  const [error, setError] = useState<{message: string} | null>(null);
+  const [error, setError] = useState<{ message: string } | null>(null);
 
   useEffect(() => {
     const fethNote = async () => {
       try {
-        const {data, error} = await getNotes();
-        const notes = data.filter(note => note.user_id === id);
+        const { data, error } = await getNotes();
+        const notes = data.filter((note: Note) => note.user_id === id);
 
-        
+
         setNotes(notes);
         setError(error);
       } catch (err) {
-        setError(err instanceof Error ? {message: err.message} : {message: 'Error al cargar la nota'});
+        setError(err instanceof Error ? { message: err.message } : { message: 'Error al cargar la nota' });
       }
     }
 
     fethNote();
   }, [id]);
 
-  return {notes, error};
+  return { notes, error };
+}
+
+export default function useCreateNote() {
+  const [formNote, setFormNote] = useState<FormData>({
+    title: '',
+    code: '',
+    explanation: '',
+    solution: '',
+    language: 'javascript',
+    tags: ''
+  });
+  const { user } = useUser();
+  const router = useRouter();
+
+
+  const postNote = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          ...formNote,
+          tags: formNote.tags.split(',').map(tag => tag.trim()).filter(tag => tag.length > 0),
+          user_id: user?.id
+        })
+      })
+
+      if (!response.ok) {
+        throw new Error("Error to save note")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+    router.refresh();
+  }
+
+  return { formNote, setFormNote, postNote }
+
 }
 
 export function useGetNote(noteId: string): UseNoteResult {
   const [note, setNote] = useState<Note | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const {user} = useUser();
+  const { user } = useUser();
   const id = user?.id;
 
   useEffect(() => {
@@ -71,4 +112,37 @@ export function useGetNote(noteId: string): UseNoteResult {
   }, [id, noteId]);
 
   return { note, loading, error };
+}
+
+export function useUpdateNote(noteId: string) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [newNote, setNewNote] = useState<FormData>({
+    title: '',
+    code: '',
+    explanation: '',
+    solution: '',
+    language: 'javascript',
+    tags: ''
+  });
+
+  const { user } = useUser();
+  const id = user?.id;
+
+  const updatedNote = async () => {
+    try {
+      setLoading(true);
+      const response = await updateNote(noteId, newNote, id as string);
+      console.log(response)
+
+      setLoading(false);
+      setError(null);
+    } catch (error) {
+      setError(error instanceof Error ? error.message : 'Error al actualizar la nota');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { updatedNote, loading, error, setNewNote };
 }
