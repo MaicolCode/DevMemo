d5125e0f9b506dd1b92a0189476e3bf54b3bb492
MaicolCode <paucarjhon07@outlook.com>
2025-05-20 16:53:38 -0500

    Creating a search component for filter notes

---
+++ src/app/ui/SearchComponent.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { SearchIcon } from "lucide-react"
+
+
+export default function SearchComponent({...props}) {
+    return (
+        <div className="flex items-center gap-2 border border-[#2a2a2a] rounded-lg px-2 text-sm">
+            <SearchIcon className="w-4 h-4" />
+            <input type="text" {...props} placeholder="Buscar..." className="w-full px-1 py-2 rounded-lg focus:outline-none" />
+        </div>
+    )
+}
