9e200719f61722a5aa9e118143c6f4ad147c37be
MaicolCode <paucarjhon07@outlook.com>
2025-05-19 20:18:15 -0500

    Updating appearance in ClerkProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      layout: {
+        unsafe_disableDevelopmentModeWarnings: true,
+      },
+    }}>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} ${fontQuestrial.className} bg-[#0f0f0f] antialiased`}
