1699024d646caa3cf49c37c09317fd20d1ac8291
MaicolCode <paucarjhon07@outlook.com>
2025-05-25 16:59:57 -0500

    Refactor styles and events in textarea element

--- src/app/ui/FormTextArea.tsx
+++ src/app/ui/FormTextArea.tsx
@@ -7,35 +7,35 @@ interface FormTextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> 
 
 export default function FormTextArea({ label, error, ...props }: FormTextAreaProps) {
     return (
-        <div className="relative space-y-1 h-auto box-border">
+        <div className="space-y-1 h-auto box-border">
             <label
                 htmlFor={props.id}
-                className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1 "
             >
                 {label}
             </label>
-            <textarea
-                {...props}
-                autoComplete='off'
-                spellCheck={false}
-                autoCapitalize='off'
-                className={`
-                    w-full p-4 h-full overflow-hidden border border-[#535353c7] rounded-md bg-[#1e1e1e] 
-                    focus:outline-none focus:ring-1 focus:ring-white 
-                    transition-all duration-200 ease-in min-h-[100px] resize-none
-                    ${error ? 'border-red-500' : ''}
-                `}
-                // Agregando un estilo dependiendo de la correspondencia del valor
-                // para que el textarea se ajuste al contenido
-                style={{ height: 'auto', minHeight: '100px' }}
-                // Evento que se ejecuta cuando se presiona una tecla
-                // para que el textarea se ajuste al contenido
-                onKeyUp={(e) => {
-                    const target = e.currentTarget as HTMLTextAreaElement;
-                    target.style.height = 'auto';
-                    target.style.height = `${target.scrollHeight}px`;
-                }}
-            />
+            <div className={`p-4 h-auto box-border overflow-hidden w-full border border-[#535353c7] rounded-md bg-[#1e1e1e] 
+                     focus-within:ring-1 focus-within:ring-white 
+                    transition-all duration-200 ease-in ${error ? 'border-red-500' : ''}`}>
+                <textarea
+                    {...props}
+                    autoComplete='off'
+                    spellCheck={false}
+                    autoCapitalize='off'
+                    className='w-full border-none p-0 m-0 h-full leading-5 tracking-wide overflow-hidden focus:outline-none resize-none min-h-[100px] max-h-[calc(100vh - 20rem)]'
+                    style={{ height: 'auto'}}
+                    onKeyDown={(e) => {
+                        const target = e.currentTarget as HTMLTextAreaElement;
+                        target.style.height = 'auto';
+                        target.style.height = `${target.scrollHeight}px`;
+                    }}
+                    onInput={(e) => {
+                        const target = e.currentTarget as HTMLTextAreaElement;
+                        target.style.height = 'auto';
+                        target.style.height = `${target.scrollHeight}px`;
+                    }}
+                />
+            </div>
             {error && (
                 <p className="mt-1 text-sm text-red-400">{error}</p>
             )}
